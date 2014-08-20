211cfe54a59994915cf9ec01e94a2263a7fbece5
czx0619 <czx0619@192.168.1.112>
2014-08-20 16:47:57 +0800

    新闻及简介页面

--- src/main/webapp/js/qlintro/qlintro.js
+++ src/main/webapp/js/qlintro/qlintro.js
@@ -7,10 +7,26 @@
 	
 	function changeColor(){
 		
-		$(".head-bottom div").each(function(i){
+		$("#qlintroLeft ul li").each(function(i){
 			$(this).click(function(){
-				$(this).siblings().attr("class","col-xs-2");
-				$(this).attr("class","col-xs-2 colorSelect");
+				$(this).siblings().attr("class","");
+				$(this).attr("class","selected");
+				var divName=$(this).attr("id");
+				if(divName=="qljj"){
+					$("#qlintro").css("display","block");
+					$("#organizeChart").css("display","none");
+					$("#rules").css("display","none");
+				}
+				else if(divName=="zzjg"){
+					$("#qlintro").css("display","none");
+					$("#organizeChart").css("display","block");
+					$("#rules").css("display","none");
+				}
+				else if(divName=="zc"){
+					$("#qlintro").css("display","none");
+					$("#organizeChart").css("display","none");
+					$("#rules").css("display","block");
+				}
 			});
 		});
 	}
