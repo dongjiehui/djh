2f0a26a9a681c8d4ac1d65016237feb1cb517b83
lihua123569 <lihua123569@163.com>
2014-08-29 11:32:58 +0800

    故乡

--- src/main/java/cn/chiv/controller/HometownFlirtatiousController.java
+++ src/main/java/cn/chiv/controller/HometownFlirtatiousController.java
@@ -3,6 +3,8 @@ package cn.chiv.controller;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,7 +34,8 @@ public class HometownFlirtatiousController extends AbstractBaseController<Hometo
 	
 	/**	表单提交 文字+图片 发布国外生活	*/
 	@RequestMapping(value = "addHometown", method = RequestMethod.POST, consumes = "multipart/form-data")
-	public void addLiveAbroad(@RequestParam String title, @RequestParam String contents, @RequestParam MultipartFile imgs) {
+	public void addLiveAbroad(@RequestParam String title, @RequestParam String contents,
+			@RequestParam MultipartFile imgs,	HttpServletResponse response) {
 		
 		try {
 			HometownFlirtatious flirtatious = new HometownFlirtatious();		
@@ -50,7 +53,14 @@ public class HometownFlirtatiousController extends AbstractBaseController<Hometo
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		try {
+			response.getWriter().write("<script type='text/javascript'>"
+					+ " top.location.href='http://localhost:8080/html/publish/publishCommon.html'"
+					+ "</script>");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**	获取图片	*/
